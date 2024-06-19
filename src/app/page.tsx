2d6f23c2da94fbf487a90a59a76a248d0f09d774
Zafar <n_zafar_007@hotmail.com>
2024-06-19 12:27:01 +0200

    unnecessary header removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,12 +39,6 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Flicker feed viewer
-        </p>
-      </div>
-
       <div className="mb-32 text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <div className="lg:col-span-3">
           <h1 className="text-3xl font-bold text-center lg:text-left lg:text-5xl">
